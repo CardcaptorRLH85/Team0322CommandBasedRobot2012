b258ceaa2663800038b3c4c87a7c3bf43408b17b
Raa'Shaun Hunter <RaaShaun.Hunter@Gmail.com>
2012-03-20 03:13:38 -0400

    Creation of a command that will automatically drop a bridge.

---
+++ Team0322CommandBasedRobot2012/Commands/AutoDropBridge.h
@@ -0,0 +1,16 @@
+#ifndef AUTODROPBRIDGE_H
+#define AUTODROPBRIDGE_H
+
+#include "Commands/CommandGroup.h"
+
+/**
+ *
+ *
+ * @author 322Programmer
+ */
+class AutoDropBridge: public CommandGroup {
+public:	
+	AutoDropBridge();
+};
+
+#endif
